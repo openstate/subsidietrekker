8d257e2d3c0d9c79511940715c59828391ccdecd
boweyism <kwbowey@gmail.com>
2016-03-30 09:28:27 +0200

    Added the needed functions to elastic.py: A means of connecting, receiving form data, building query, handing off results

--- app/elastic.py
+++ app/elastic.py
@@ -6,7 +6,32 @@ Elastic.py
 '''
 from elasticsearch import Elasticsearch
 
+
+# connect to ES and create ElasticSearch instance
 ES_CLUSTER = 'http://localhost:9200'
 ES_INDEX = 'sub'
 ES_TYPE = 'item'
 es = Elasticsearch(ES_CLUSTER)
+
+#receive input from form
+def receiver(form_data):
+    pass
+
+
+# build ES query using cleaned data from receiver
+def query_builder(cleaned_data):
+    pass
+
+
+#query ES
+def query_es(query_data):
+    pass
+
+
+# pass query results to front
+def pass_to_table(query_results):
+    pass
+
+
+def pass_to_viz(query_results):
+    pass
